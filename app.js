c62a8b75cc0624119b3d32d2e549c5fd320d5572
apocalipse51 <gabrielantonio20111@hotmail.com>
2018-05-17 18:01:04 -0300

    Https Fix

--- app.js
+++ app.js
@@ -21,15 +21,13 @@ app.use(function(req, res, next) {
     next(createError(404));
 });
 
-app.configure('production', () => {
-  	app.use((req, res, next) => {
-	    if (req.header('x-forwarded-proto') !== 'https') {
-	      res.redirect(`https://${req.header('host')}${req.url}`);
-	    } else {
-	      next();
-	    }
-  	})
-});
+app.use((req, res, next) => {
+	if (req.header('x-forwarded-proto') !== 'https') {
+	  res.redirect(`https://${req.header('host')}${req.url}`);
+	} else {
+	  next();
+	}
+})
 
 app.get('/', (req, res, next) => {
     res.render('index');
