6b47107d7f0a04852d4a3842ed878c0ad033b136
apocalipse51 <gabrielantonio20111@hotmail.com>
2018-05-17 17:51:29 -0300

    Redirect http to https

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ app.use(function(req, res, next) {
     next(createError(404));
 });
 
+app.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+});
+
 app.get('/', (req, res, next) => {
     res.render('index');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
+app.use(function(err, req, res, next) {    
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
